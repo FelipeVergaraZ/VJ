de202ce54f4458b8d7e484ffc64d9b449ed10cf8
johncruzsanchez <jo.cruzs@duocuc.cl>
2021-05-24 19:23:37 -0400

    cambio2

--- pruebaDjango/formulario/views.py
+++ pruebaDjango/formulario/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
 
-    return render(request, 'formulario/index.html')
+    return render(request, 'wikigame/index.html')
 
+def items(request):
+
+    return render(request, 'wikigame/items.html')
 
