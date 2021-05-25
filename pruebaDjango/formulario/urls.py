b2c2ab9938f1a0ccb44feccb426a3cb1abcda195
johncruzsanchez <jo.cruzs@duocuc.cl>
2021-05-25 14:24:09 -0400

    cambio django de las paginas 25/05 14:20

--- pruebaDjango/formulario/urls.py
+++ pruebaDjango/formulario/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from .views import home, items
-from .views import personajes
+from .views import personajes, contact
+from .views import enemigos, mercancia
 
 urlpatterns = [
     path('',home,name='home'),
     path('personajes/',personajes,name='personajes'),
+    path('enemigos/',enemigos,name='enemigos'),
+    path('contact/',contact,name='contact'),
+    path('mercancia/',mercancia,name='mercancia'),
     path('items/',items,name='items')
     
 ]
