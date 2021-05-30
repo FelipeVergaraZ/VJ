585cb81e270bce74ddf9cf217334db88b49ff5e9
FelipeVergaraz <fel.vergaraz@duocuc.cl>
2021-05-30 17:29:28 -0400

    actualizacion

--- pruebaDjango/formulario/views.py
+++ pruebaDjango/formulario/views.py
@@ -2,10 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'formulario/index.html')
+    return render(request, 'xartgord/index.html')
+
+def personajes(request):
+    return render(request, 'xartgord/personajes.html')
 
 def items(request):
-    return render(request, 'formulario/items.html')
+    return render(request, 'xartgord/items.html')
 
+def enemigos(request):
+    return render(request, 'xartgord/enemigos.html')
+
+def contact(request):
+    return render(request, 'xartgord/contact.html')
+
+def mercancia(request):
+    return render(request, 'xartgord/mercancia.html')
 
 
