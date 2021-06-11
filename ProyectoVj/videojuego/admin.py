c7578b17c76d293721ce99032d77330ff957a74b
FelipeVergaraz <fel.vergaraz@duocuc.cl>
2021-06-11 14:17:45 -0400

    VJ

--- ProyectoVj/videojuego/admin.py
+++ ProyectoVj/videojuego/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Rango,Usuario,Mercancia
 
 # Register your models here.
+admin.site.register(Rango)
+admin.site.register(Usuario)
+admin.site.register(Mercancia)
--- ProyectoVj/videojuego/models.py
+++ ProyectoVj/videojuego/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db.models.fields import CharField
 
 # Create your models here.
 class Rango(models.Model):
@@ -11,20 +10,21 @@ def __str__(self):
 
 
 class Usuario(models.Model):
-    idusuario=models.IntegerField(primary_key=True,verbose_name='Id de Usuario')
+    idUsuario=models.IntegerField(primary_key=True,verbose_name='Id de Usuario')
     nombreusuario=models.CharField(max_length=20,verbose_name='Nombre del usuario')
     contraseñausuario=models.CharField(max_length=20,verbose_name='Contraseña del usuario')
     rango=models.ForeignKey(Rango, on_delete=models.CASCADE)
 
 def __str__(self):
-    return self.idusuario
+    return self.nombreusuario
 
 class Mercancia(models.Model):
     idproducto=models.IntegerField(primary_key=True,verbose_name='Id del Producto')
     nombreproducto=models.CharField(max_length=20,verbose_name='Nombre del Producto')
     stockproducto=models.IntegerField(verbose_name='Stock del producto')
+    precioproducto=models.IntegerField(verbose_name='Precio del producto')
 
 def __str__(self):
-    return self.idproducto
+    return self.nombreproducto
 
 
